26da46b5569fdf2c57b8583d6ca2f33728f44e7f
Matt Lewis <matt@mplewis.com>
2022-07-29 14:43:42 -0600

    fix: spec shows more sane behavior

--- lookup/args_test.go
+++ lookup/args_test.go
@@ -8,19 +8,28 @@ import (
 )
 
 var _ = Describe("NewFromArgs", func() {
-	It("works as intended", func() {
-		in := []string{
+	It("parses config values from os.Args", func() {
+		args := []string{
 			"app.go",
-			"--foo=bar", // only known arg format is --a=b
+			"--check-interval=0.02", // only known arg format is --a=b
 			"-test",
-			"--baz=quux corge xyzzy", // supports quoted shell values
+			"--success-message=Your application was submitted", // supports quoted shell values
 			"--xxx",
 			"yyy",
 			"some-pos-arg",
-			"--foo=grault", // later overrides earlier
+			"--check-interval=0.05", // later overrides earlier
 		}
-		cfg := lookup.NewFromArgs(in)
+		cfg := lookup.NewFromArgs(args)
+
 		_, found := cfg.Get("test")
 		Expect(found).To(BeFalse())
+
+		val, found := cfg.Get("check_interval")
+		Expect(found).To(BeTrue())
+		Expect(val).To(Equal("0.05"))
+
+		val, found = cfg.Get("success_message")
+		Expect(found).To(BeTrue())
+		Expect(val).To(Equal("Your application was submitted"))
 	})
 })
